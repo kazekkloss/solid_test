29c4970d7032a2fef5985a4c60ddea5be848ed64
kazekkloss <kazekkloss.kr@gmai.com>
2025-09-25 18:04:32 +0200

    fixed code in color_generator

--- lib/core/methods/color_generator.dart
+++ lib/core/methods/color_generator.dart
@@ -4,12 +4,15 @@ import 'package:flutter/material.dart';
 class ColorGenerator {
   final Random _random = Random();
 
+  static const int _fullOpacity = 255;
+  static const int _maxRGB = 256;
+
   Color getRandomColor() {
     return Color.fromARGB(
-      255,
-      _random.nextInt(256),
-      _random.nextInt(256),
-      _random.nextInt(256),
+      _fullOpacity,
+      _random.nextInt(_maxRGB),
+      _random.nextInt(_maxRGB),
+      _random.nextInt(_maxRGB),
     );
   }
 }
